a55b9b500c7c67f8d872cd21e2c4640b71654cd1
meeram2757-pixel <meeram2757@gmail.com>
2026-03-05 22:07:06 +0500

    commit

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -26,9 +26,10 @@ const LandingPage = () => {
             We are changing the way people shop
           </h1>
           <p className="mt-8 max-w-xl text-lg leading-8">
- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore repellat explicabo enim soluta temporibus asperiores aut obcaecati perferendis porro nobis.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore repellat explicabo 
+            enim soluta temporibus asperiores aut obcaecati perferendis porro nobis.
 
-         </p>
+          </p>
           <div className="mt-10">
             <Link to="/products" className="btn btn-primary">
               OUR PRODUCTS
